b42f472d8437b9af27a0baefdc7455b87fb10edf
Nikhit-dodia <nikhitdodia2701@gmail.com>
2025-12-02 22:48:18 +0530

    "updated v4"

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,26 +5,7 @@ export default function Experience() {
       <h2>Experience</h2>
       <h3> Tata Consultancy Services (TCS)</h3>
       <p>Software Engineer (March 2024 - Present)</p>
-      <p>
-        <b>
-          AI Cloud Unit – HSBC Banking (Pune) Frontend Developer / Power
-          Platform Trainee (Mar–Jun 2025)
-        </b>
-      </p>
-      <p>
-        Worked on enterprise-level applications within the Microsoft Power
-        Platform ecosystem, focusing on Power Apps UI customizations and
-        frontend layout improvements.
-      </p>
-      <p>
-        Designed and modified screens, forms, interface flows, and user
-        experiences for internal HSBC banking operations. Collaborated with
-        cloud and automation teams using SharePoint and Power Automate to
-        streamline internal workflows. Improved existing app usability with
-        cleaner layouts, consistent UI patterns, and user-centered changes.
-        Gained experience handling secure banking environments, versioning, and
-        controlled deployments.
-      </p>
+     
       <p>
         <b>
           Cloud – Healthcare & Life Sciences Unit (Eli Lilly) Digital Design &
@@ -48,6 +29,28 @@ export default function Experience() {
         Building cross-disciplinary skills connecting design + marketing +
         technology for global healthcare communication.
       </p>
+
+      <p>
+        <b>
+          AI Cloud Unit – HSBC Banking (Pune) Frontend Developer / Power
+          Platform Trainee (Mar–Jun 2025)
+        </b>
+      </p>
+      <p>
+        Worked on enterprise-level applications within the Microsoft Power
+        Platform ecosystem, focusing on Power Apps UI customizations and
+        frontend layout improvements.
+      </p>
+      <p>
+        Designed and modified screens, forms, interface flows, and user
+        experiences for internal HSBC banking operations. Collaborated with
+        cloud and automation teams using SharePoint and Power Automate to
+        streamline internal workflows. Improved existing app usability with
+        cleaner layouts, consistent UI patterns, and user-centered changes.
+        Gained experience handling secure banking environments, versioning, and
+        controlled deployments.
+      </p>
+
     </section>
   );
 }
